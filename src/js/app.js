547ba3cc16de22d7701a3dce872fd65b5a9ae00a
t1234 <meinGitLabor42>
2018-09-07 12:43:13 +0200

    some DB stuff

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,42 @@ const express = require('express');
 
 const app = express();
 
+const addressbooks = [
+  { name: 'Hospitals' },
+  { name: 'Schools' },
+  { name: 'Restaurants' },
+];
+const groups = [
+    { name: 'Bone Medicine' },
+    { name: 'Teeth Medicine' },
+    { name: 'Pre Schools' },
+    { name: 'High Schools' },
+    { name: 'Fish Restaurants' },
+    { name: 'Meat Restaurants' },
+];
+
+
+
+
+// we now can assume the api key is valid,
+// and simply expose the data
+
+// example: http://localhost:3000/api/users/?api-key=foo
+app.get('/api/addressbooks/', (req, res, next) => {
+  res.send(addressbooks);
+});
+
+// example: http://localhost:3000/api/users/?api-key=foo
+app.get('/api/groups/', (req, res, next) => {
+    res.send(groups);
+});
+
+// example: http://localhost:3000/api/repos/?api-key=foo
+app.get('/api/repos', (req, res, next) => {
+  res.send("repos");
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
