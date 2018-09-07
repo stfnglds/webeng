be3abf8e771da0c713920fe2c9ec69c0382ba88f
t1234 <meinGitLabor42>
2018-09-07 14:02:11 +0200

    start server in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp');
 const jsdoc = require('gulp-jsdoc3');
-//const bootstrap = require('bootstrap');
+const nodemon = require('gulp-nodemon');
+// const bootstrap = require('bootstrap');
 
 gulp.task('default', (cb) => {
   gulp.src(['README.md', './src/**/*.js'], { read: false })
     .pipe(jsdoc(cb));
+});
+
+gulp.task('start server', () => {
+  nodemon({
+    script: './src/js/app.js',
+    ext: 'js html',
+    env: { NODE_ENV: 'development' },
+  });
 });
