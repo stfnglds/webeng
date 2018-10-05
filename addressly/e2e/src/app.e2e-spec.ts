5b55a0d3e1e5e2674a72c252c2f079c548a356ad
t1234 <meinGitLabor42>
2018-10-05 15:17:22 +0200

    e2e test

--- addressly/e2e/src/app.e2e-spec.ts
+++ addressly/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
-import { AppPage } from './app.po';
+import {AppPage} from './app.po';
+import {$, $$, browser, by, element} from "protractor";
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -7,8 +8,63 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  //Simple Router tests
+  it('should change URL when navbar is clicked', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to addressly!');
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/');
+
+    page.getNavbarEntriesButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/entries');
+
+    page.getNavbarGroupsButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/groups');
+
+    page.getNavbarAddressbooksButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/addressbooks');
+
+    //make sure routing works from different pages
+
+    page.getNavbarEntriesButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/entries');
+
+    page.getNavbarGroupsButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/groups');
+
+    page.getNavbarAddressbooksButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/addressbooks');
+  });
+
+  //Simple Router tests
+  it('should create a new entry', async () => {
+    page.navigateTo();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/');
+    page.getNavbarEntriesButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/entries');
+
+
+    var entryCount = await $$('app-entry-card').count();
+
+    page.getNewEntryButton().click();
+    const newNameString1 = "Random 1234 :_$§";
+    const newNameString2 = "1234 :_$§ Random";
+    page.getNewEntryNameInput().sendKeys(newNameString1);
+    page.getNewEntryAddressInput().sendKeys(newNameString2);
+    page.getNewEntrySaveButton().click();
+
+    expect(page.getEntryCards().count()).toBe(entryCount + 1);
+  });
+
+  it('should delete an entry', async () => {
+    page.navigateTo();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/');
+    page.getNavbarEntriesButton().click();
+    expect(browser.getCurrentUrl()).toEqual('http://localhost:4200/entries');
+
+    var entryCount = await $$('app-entry-card').count();
+    $$(".deleteButton").last().click();
+
+    $$(".reallyDeleteEntryButton").last().click();
+    expect(page.getEntryCards().count()).toBe(entryCount - 1);
   });
 });
+
--- addressly/e2e/src/app.po.ts
+++ addressly/e2e/src/app.po.ts
@@ -8,4 +8,31 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+  getNavbarEntriesButton(){
+    return element(by.className('navbarEntriesButton'));
+  }
+  getNavbarGroupsButton(){
+    return element(by.className('navbarGroupsButton'));
+  }
+  getNavbarAddressbooksButton(){
+    return element(by.className('navbarAddressbooksButton'));
+  }
+  getNewEntryButton(){
+    return element(by.className('newEntryButton'));
+  }
+  getNewEntryAddressInput(){
+    return element(by.className('entryInputAddress'));
+  }
+  getNewEntryNameInput(){
+    return element(by.className('entryInputName'));
+  }
+  getNewEntrySaveButton(){
+    return element(by.className('entrySaveButton'));
+  }
+  getEntryCardParent(){
+    return element(by.className('entryCardsContainer'));
+  }
+  getEntryCards(){
+    return element.all(by.className('card-body'));
+  }
 }
