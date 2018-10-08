8e9dfa786d060d198c42ebe96b4db34e54ced819
MelZeem <39190524+MelZeem@users.noreply.github.com>
2018-10-08 12:22:28 +0200

    Create TESTgetAllEntries.js

---
+++ src/db/TESTgetAllEntries.js
@@ -0,0 +1,9 @@
+const Engine = require('tingodb')();
+
+const db = new Engine.Db('', {});
+
+const collection = db.collection('entries');
+
+collection.find({}).toArray((error, list) => {
+    console.log(list);
+});
