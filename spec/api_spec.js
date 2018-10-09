d7ec5e611ffa4325c630b8157bea3c321cb6f78f
t1234 <meinGitLabor42>
2018-10-09 07:49:34 +0200

    jasmine API Server Codes Test

--- spec/api_spec.js
+++ spec/api_spec.js
@@ -3,45 +3,150 @@ const api = require('../src/services/api.js');
 
 // IMPORTANT: Stop API Node server before running gulp jasmine
 
-// describe('API Tests', () => {
-//     it('contains spec with an expectation', async () => {
-//         let list = [];
-//         await (function () {
-//             list = api.getAllEntries();
-//         });
-//
-//         expect(list.length).toBeGreaterThan(0);
-//     });
-// });
-
-
 describe('GET', () => {
     it('entries', (done) => {
         request('http://localhost:3000/api/entries', (error, response, body) => {
-            var obj = JSON.parse(body);
-            expect(obj.length).toBeGreaterThan(0);
+            const obj = JSON.parse(body);
+            expect(obj.length)
+                .toBeGreaterThan(0);
             done();
         });
     });
     it('entrie 1 has a name', (done) => {
         request('http://localhost:3000/api/entries', (error, response, body) => {
-            var obj = JSON.parse(body);
-            expect(obj[0].name).toBeDefined();
+            const obj = JSON.parse(body);
+            expect(obj[0].name)
+                .toBeDefined();
             done();
         });
     });
     it('groups', (done) => {
         request('http://localhost:3000/api/groups', (error, response, body) => {
-            var obj = JSON.parse(body);
-            expect(obj.length).toBeGreaterThan(0);
+            const obj = JSON.parse(body);
+            expect(obj.length)
+                .toBeGreaterThan(0);
             done();
         });
     });
     it('addressbooks', (done) => {
         request('http://localhost:3000/api/addressbooks', (error, response, body) => {
-            var obj = JSON.parse(body);
-            expect(obj.length).toBeGreaterThan(0);
+            const obj = JSON.parse(body);
+            expect(obj.length)
+                .toBeGreaterThan(0);
             done();
         });
     });
 });
+
+describe('Server Codes', () => {
+
+    describe('Entries', () => {
+        beforeEach(function () {
+            url = 'http://localhost:3000/api/entries';
+        });
+
+        it('should return 200 on GET', function (done) {
+            request.get(url, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(200);
+                done();
+            });
+        });
+
+        it('should fail on POST', function (done) {
+            request.post(url, {
+                json: true,
+                body: {},
+            }, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(500);
+                done();
+            });
+        });
+
+        it('should give 200 on POST', function (done) {
+            request.post(url, {
+                json: true,
+                body: { name: 'test' },
+            }, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(200);
+                done();
+            });
+        });
+    });
+
+    describe('Groups', () => {
+        beforeEach(function () {
+            url = 'http://localhost:3000/api/groups';
+        });
+
+        it('should return 200 on GET', function (done) {
+            request.get(url, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(200);
+                done();
+            });
+        });
+
+        it('should fail on POST', function (done) {
+            request.post(url, {
+                json: true,
+                body: {},
+            }, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(500);
+                done();
+            });
+        });
+
+        it('should give 200 on POST', function (done) {
+            request.post(url, {
+                json: true,
+                body: { name: 'test' },
+            }, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(200);
+                done();
+            });
+        });
+    });
+
+    describe('Addressbooks', () => {
+        beforeEach(function () {
+            url = 'http://localhost:3000/api/addressbooks';
+        });
+
+        it('should return 200 on GET', function (done) {
+            request.get(url, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(200);
+                done();
+            });
+        });
+
+        it('should fail on POST', function (done) {
+            request.post(url, {
+                json: true,
+                body: {},
+            }, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(500);
+                done();
+            });
+        });
+
+        it('should give 200 on POST', function (done) {
+            request.post(url, {
+                json: true,
+                body: { name: 'test' },
+            }, function (error, response) {
+                expect(response.statusCode)
+                    .toEqual(200);
+                done();
+            });
+        });
+    });
+});
+
+
